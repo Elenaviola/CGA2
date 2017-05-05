f61b9ab843d9b775326aff8bcd334c28f5148ceb
FabianFriederichs <fabian.friederichs@gmail.com>
2017-05-05 17:06:25 +0200

    Changed name of a macro to prevent name collisions

--- CMakeProject/framework/fw_config.h
+++ CMakeProject/framework/fw_config.h
@@ -5,6 +5,6 @@
 #define THROW_ON_GL_ERROR 1		//throw exception when OpenGL error occures
 #define HOLD_ON_GL_ERROR 0		//print error and wait for a key when OpenGL error occures
 #define LOG_GL_ERRORS 1			//Log all errors to "glerrorlog.txt"
-#define GLDEBUG					//if not defined, GLERR does nothing
+#define CGA2_DEBUG					//if not defined, GLERR does nothing
 
 #endif
